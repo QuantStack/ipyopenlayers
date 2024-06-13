1a10c93f1e98e64e5789ea5aa0f46d739158b41e
martinRenou <martin.renou@gmail.com>
2024-06-13 09:22:00 +0100

    Add ui-tests (#11)
    * Add ui-tests
    
    * Update
    
    * Oups
    
    * Yarn install missing
    
    * Use jlpm not yarn
    
    * Missing jupyterlab dep
    
    * Add snapshots
    
    * Only light theme for now

---
+++ ui-tests/jupyter_server_config.py
@@ -0,0 +1,5 @@
+from jupyterlab.galata import configure_jupyter_server
+
+configure_jupyter_server(c)  # noqa F821
+# Uncomment to set server log level to debug level
+# c.ServerApp.log_level = "DEBUG"
