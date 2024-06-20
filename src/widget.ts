8edad88e01be1d588b91117618b09d49d58673d1
Nour Cheour <161816839+Nour-Cheour10@users.noreply.github.com>
2024-06-20 14:19:09 +0200

    change tilelayer to layer

--- src/widget.ts
+++ src/widget.ts
@@ -7,7 +7,7 @@ import {
   unpack_models,
   ViewList,
 } from '@jupyter-widgets/base';
-import { TileLayerModel, TileLayerView } from './tilelayer';
+import { LayerModel, LayerView } from './layer';
 import { BaseOverlayModel, BaseOverlayView } from './baseoverlay';
 import { BaseControlModel, BaseControlView } from './basecontrol';
 import { ObjectEvent } from 'ol/Object';
@@ -123,7 +123,7 @@ export class MapView extends DOMWidgetView {
   }
 
   layersChanged() {
-    const layers = this.model.get('layers') as TileLayerModel[];
+    const layers = this.model.get('layers') as LayerModel[];
     this.layerViews.update(layers);
   }
 
@@ -151,7 +151,7 @@ export class MapView extends DOMWidgetView {
     }
   }
 
-  removeLayerView(child_view: TileLayerView) {
+  removeLayerView(child_view: LayerView) {
     this.map.removeLayer(child_view.obj);
     child_view.remove();
   }
@@ -168,8 +168,8 @@ export class MapView extends DOMWidgetView {
     child_view.remove();
   }
 
-  async addLayerModel(child_model: TileLayerModel) {
-    const view = await this.create_child_view<TileLayerView>(child_model, {
+  async addLayerModel(child_model: LayerModel) {
+    const view = await this.create_child_view<LayerView>(child_model, {
       map_view: this,
     });
     this.map.addLayer(view.obj);
@@ -208,7 +208,7 @@ export class MapView extends DOMWidgetView {
   imageElement: HTMLImageElement;
   mapContainer: HTMLDivElement;
   map: Map;
-  layerViews: ViewList<TileLayerView>;
+  layerViews: ViewList<LayerView>;
   overlayViews: ViewList<BaseOverlayView>;
   controlViews: ViewList<BaseControlView>;
 }
