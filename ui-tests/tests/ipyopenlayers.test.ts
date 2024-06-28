a15ce7bd0cec164a0e532b724d0aad23a39e671a
Nour-Cheour10 <nour.cheour@insat.ucar.tn>
2024-06-28 11:29:59 +0200

    await layer

--- ui-tests/tests/ipyopenlayers.test.ts
+++ ui-tests/tests/ipyopenlayers.test.ts
@@ -5,159 +5,166 @@ import { expect, IJupyterLabPageFixture, test } from '@jupyterlab/galata';
 import * as path from 'path';
 const klaw = require('klaw-sync');
 
+async function waitForLayerRendering(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const filterUpdateNotebooks = (item) => {
-  const basename = path.basename(item.path);
-  return basename.includes('_update');
+    const basename = path.basename(item.path);
+    return basename.includes('_update');
 };
 
 const testCellOutputs = async (
-  page: IJupyterLabPageFixture,
-  tmpPath: string,
-  theme: 'JupyterLab Light' | 'JupyterLab Dark',
+    page: IJupyterLabPageFixture,
+    tmpPath: string,
+    theme: 'JupyterLab Light' | 'JupyterLab Dark',
 ) => {
-  const paths = klaw(path.resolve(__dirname, './notebooks'), {
-    filter: (item) => !filterUpdateNotebooks(item),
-    nodir: true,
-  });
-  const notebooks = paths.map((item) => path.basename(item.path));
-
-  const contextPrefix = theme == 'JupyterLab Dark' ? 'light' : 'dark';
-  if (theme == 'JupyterLab Dark') {
-    page.theme.setTheme(theme);
-  }
-
-  for (const notebook of notebooks) {
-    let results = [];
-
-    await page.notebook.openByPath(`${tmpPath}/${notebook}`);
-    await page.notebook.activate(notebook);
-
-    let numCellImages = 0;
-
-    const getCaptureImageName = (
-      contextPrefix: string,
-      notebook: string,
-      id: number,
-    ): string => {
-      return `${contextPrefix}-${notebook}-cell-${id}.png`;
-    };
-
-    await page.notebook.runCellByCell({
-      onAfterCellRun: async (cellIndex: number) => {
-        const cell = await page.notebook.getCellOutput(cellIndex);
-        if (cell) {
-          results.push(await cell.screenshot());
-          numCellImages++;
-        }
-      },
+    const paths = klaw(path.resolve(__dirname, './notebooks'), {
+        filter: (item) => !filterUpdateNotebooks(item),
+        nodir: true,
     });
+    const notebooks = paths.map((item) => path.basename(item.path));
 
-    await page.notebook.save();
-
-    for (let c = 0; c < numCellImages; ++c) {
-      expect(results[c]).toMatchSnapshot(
-        getCaptureImageName(contextPrefix, notebook, c),
-        { threshold: 0.3 },
-      );
+    const contextPrefix = theme == 'JupyterLab Dark' ? 'light' : 'dark';
+    if (theme == 'JupyterLab Dark') {
+        page.theme.setTheme(theme);
     }
 
-    await page.notebook.close(true);
-  }
+    for (const notebook of notebooks) {
+        let results = [];
+
+        await page.notebook.openByPath(`${tmpPath}/${notebook}`);
+        await page.notebook.activate(notebook);
+
+        let numCellImages = 0;
+
+        const getCaptureImageName = (
+            contextPrefix: string,
+            notebook: string,
+            id: number,
+        ): string => {
+            return `${contextPrefix}-${notebook}-cell-${id}.png`;
+        };
+
+        await page.notebook.runCellByCell({
+            onAfterCellRun: async (cellIndex: number) => {
+                const cell = await page.notebook.getCellOutput(cellIndex);
+                if (cell) {
+                    results.push(await cell.screenshot());
+                    numCellImages++;
+                }
+            },
+        });
+
+        await page.notebook.save();
+
+        for (let c = 0; c < numCellImages; ++c) {
+            expect(results[c]).toMatchSnapshot(
+                getCaptureImageName(contextPrefix, notebook, c),
+                { threshold: 0.3 },
+            );
+        }
+
+        await page.notebook.close(true);
+    }
 };
 
 const testPlotUpdates = async (
-  page: IJupyterLabPageFixture,
-  tmpPath: string,
-  theme: 'JupyterLab Light' | 'JupyterLab Dark',
+    page: IJupyterLabPageFixture,
+    tmpPath: string,
+    theme: 'JupyterLab Light' | 'JupyterLab Dark',
 ) => {
-  const paths = klaw(path.resolve(__dirname, './notebooks'), {
-    filter: (item) => filterUpdateNotebooks(item),
-    nodir: true,
-  });
-  const notebooks = paths.map((item) => path.basename(item.path));
-
-  const contextPrefix = theme == 'JupyterLab Light' ? 'light' : 'dark';
-  if (theme == 'JupyterLab Dark') {
-    page.theme.setTheme(theme);
-  }
-
-  for (const notebook of notebooks) {
-    let results = [];
-
-    await page.notebook.openByPath(`${tmpPath}/${notebook}`);
-    await page.notebook.activate(notebook);
-
-    const getCaptureImageName = (
-      contextPrefix: string,
-      notebook: string,
-      id: number,
-    ): string => {
-      return `${contextPrefix}-${notebook}-cell-${id}.png`;
-    };
-
-    let cellCount = 0;
-    await page.notebook.runCellByCell({
-      onAfterCellRun: async (cellIndex: number) => {
-
-        const cell = await page.notebook.getCellOutput(0);
-        if (cell) {
-          results.push(await cell.screenshot());
-          cellCount++;
-        }
-      },
+    const paths = klaw(path.resolve(__dirname, './notebooks'), {
+        filter: (item) => filterUpdateNotebooks(item),
+        nodir: true,
     });
-    await page.waitForTimeout(1000); 
-    await page.notebook.save();
+    const notebooks = paths.map((item) => path.basename(item.path));
 
-    for (let i = 0; i < cellCount; i++) {
-      expect(results[i]).toMatchSnapshot(
-        getCaptureImageName(contextPrefix, notebook, i),
-        { threshold: 0.3 },
-      );
+    const contextPrefix = theme == 'JupyterLab Light' ? 'light' : 'dark';
+    if (theme == 'JupyterLab Dark') {
+        page.theme.setTheme(theme);
     }
 
-    await page.notebook.close(true);
-  }
+    for (const notebook of notebooks) {
+        let results = [];
+
+        await page.notebook.openByPath(`${tmpPath}/${notebook}`);
+        await page.notebook.activate(notebook);
+
+        const getCaptureImageName = (
+            contextPrefix: string,
+            notebook: string,
+            id: number,
+        ): string => {
+            return `${contextPrefix}-${notebook}-cell-${id}.png`;
+        };
+
+        let cellCount = 0;
+        await page.notebook.runCellByCell({
+            onAfterCellRun: async (cellIndex: number) => {
+
+                const cell = await page.notebook.getCellOutput(0);
+                if (cell) {
+                    results.push(await cell.screenshot());
+                    cellCount++;
+                }
+            },
+        });
+
+        // Adding a delay to wait for layer rendering after actions
+        await waitForLayerRendering(2000); // Adjust time as needed (2 seconds here)
+
+        await page.notebook.save();
+
+        for (let i = 0; i < cellCount; i++) {
+            expect(results[i]).toMatchSnapshot(
+                getCaptureImageName(contextPrefix, notebook, i),
+                { threshold: 0.3 },
+            );
+        }
+
+        await page.notebook.close(true);
+    }
 };
 
 test.describe('ipyopenlayers Visual Regression', () => {
-  test.beforeEach(async ({ page, tmpPath }) => {
-    page.on('console', (message) => {
-      console.log('CONSOLE MSG ---', message.text());
+    test.beforeEach(async ({ page, tmpPath }) => {
+        page.on('console', (message) => {
+            console.log('CONSOLE MSG ---', message.text());
+        });
+
+        await page.contents.uploadDirectory(
+            path.resolve(__dirname, './notebooks'),
+            tmpPath,
+        );
+        await page.filebrowser.openDirectory(tmpPath);
     });
 
-    await page.contents.uploadDirectory(
-      path.resolve(__dirname, './notebooks'),
-      tmpPath,
-    );
-    await page.filebrowser.openDirectory(tmpPath);
-  });
+    test('Light theme: Check ipyopenlayers first renders', async ({
+        page,
+        tmpPath,
+    }) => {
+        await testCellOutputs(page, tmpPath, 'JupyterLab Light');
+    });
 
-  test('Light theme: Check ipyopenlayers first renders', async ({
-    page,
-    tmpPath,
-  }) => {
-    await testCellOutputs(page, tmpPath, 'JupyterLab Light');
-  });
+    // test('Dark theme: Check ipyopenlayers first renders', async ({
+    //   page,
+    //   tmpPath,
+    // }) => {
+    //   await testCellOutputs(page, tmpPath, 'JupyterLab Dark');
+    // });
 
-  // test('Dark theme: Check ipyopenlayers first renders', async ({
-  //   page,
-  //   tmpPath,
-  // }) => {
-  //   await testCellOutputs(page, tmpPath, 'JupyterLab Dark');
-  // });
+    test('Light theme: Check ipyopenlayers update plot properties', async ({
+        page,
+        tmpPath,
+    }) => {
+        await testPlotUpdates(page, tmpPath, 'JupyterLab Light');
+    });
 
-  test('Light theme: Check ipyopenlayers update plot properties', async ({
-    page,
-    tmpPath,
-  }) => {
-    await testPlotUpdates(page, tmpPath, 'JupyterLab Light');
-  });
-
-  // test('Dark theme: Check ipyopenlayers update plot properties', async ({
-  //   page,
-  //   tmpPath,
-  // }) => {
-  //   await testPlotUpdates(page, tmpPath, 'JupyterLab Dark');
-  // });
+    // test('Dark theme: Check ipyopenlayers update plot properties', async ({
+    //   page,
+    //   tmpPath,
+    // }) => {
+    //   await testPlotUpdates(page, tmpPath, 'JupyterLab Dark');
+    // });
 });
