e1721d712302f02b5dc26d52a56b654d4f6e4aa6
Nour-Cheour10 <nour.cheour@insat.ucar.tn>
2024-07-23 12:02:13 +0200

    ApiKey

--- src/geotifflayer.ts
+++ src/geotifflayer.ts
@@ -7,6 +7,7 @@ import { MODULE_NAME, MODULE_VERSION } from './version';
 import { LayerModel, LayerView } from './layer';
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 export class GeoTIFFTileLayerModel extends LayerModel {
   defaults() {
     return {
@@ -66,57 +67,68 @@ export class GeoTIFFTileLayerView extends LayerView {
   obj: WebGLTileLayer;
 =======
 
+=======
+>>>>>>> 06de91b (ApiKey)
 export class GeoTIFFTileLayerModel extends LayerModel {
- defaults() {
-   return {
-     ...super.defaults(),
-     _model_name: GeoTIFFTileLayerModel.model_name,
-     _model_module: GeoTIFFTileLayerModel.model_module,
-     _model_module_version: GeoTIFFTileLayerModel.model_module_version,
-     _view_name: GeoTIFFTileLayerModel.view_name,
-     _view_module: GeoTIFFTileLayerModel.view_module,
-     _view_module_version: GeoTIFFTileLayerModel.view_module_version,
-     source: [],
-   };
- }
+  defaults() {
+    return {
+      ...super.defaults(),
+      _model_name: GeoTIFFTileLayerModel.model_name,
+      _model_module: GeoTIFFTileLayerModel.model_module,
+      _model_module_version: GeoTIFFTileLayerModel.model_module_version,
+      _view_name: GeoTIFFTileLayerModel.view_name,
+      _view_module: GeoTIFFTileLayerModel.view_module,
+      _view_module_version: GeoTIFFTileLayerModel.view_module_version,
+      source: [],
+    };
+  }
 
+  static serializers: ISerializers = {
+    ...DOMWidgetModel.serializers,
+    // Add any extra serializers here
+  };
 
- static serializers: ISerializers = {
-   ...DOMWidgetModel.serializers,
-   // Add any extra serializers here
- };
-
-
- static model_name = 'GeoTIFFTileLayerModel';
- static model_module = MODULE_NAME;
- static model_module_version = MODULE_VERSION;
- static view_name = 'GeoTIFFTileLayerView';
- static view_module = MODULE_NAME;
- static view_module_version = MODULE_VERSION;
+  static model_name = 'GeoTIFFTileLayerModel';
+  static model_module = MODULE_NAME;
+  static model_module_version = MODULE_VERSION;
+  static view_name = 'GeoTIFFTileLayerView';
+  static view_module = MODULE_NAME;
+  static view_module_version = MODULE_VERSION;
 }
 
-
 export class GeoTIFFTileLayerView extends LayerView {
- render() {
-   super.render();
-   this.sourcesChanged();
-   this.model.on('change:sources', this.sourcesChanged, this);
- }
+  render() {
+    super.render();
+    this.sourcesChanged();
+    this.model.on('change:sources', this.sourcesChanged, this);
+  }
 
+  create_obj() {
+    const sources = this.model.get('sources').map((source: any) => ({
+      url: source.url,
+    }));
 
- create_obj() {
-   const sources = this.model.get('sources').map((source: any) => ({
-     url: source.url,
-   }));
+    this.obj = this.tileLayer = new WebGLTileLayer({
+      source: new GeoTIFF({
+        sources: sources,
+      }),
+    });
+  }
 
+  sourcesChanged() {
+    const newSources = this.model.get('sources').map((source: any) => ({
+      url: source.url,
+    }));
 
-   this.obj = this.tileLayer = new WebGLTileLayer({
-     source: new GeoTIFF({
-       sources: sources,
-     }),
-   });
- }
+    if (newSources) {
+      const newSource = new GeoTIFF({
+        sources: newSources,
+      });
+      this.tileLayer.setSource(newSource);
+    }
+  }
 
+<<<<<<< HEAD
 
  sourcesChanged() {
    const newSources = this.model.get('sources').map((source: any) => ({
@@ -135,4 +147,7 @@ export class GeoTIFFTileLayerView extends LayerView {
 
  tileLayer: WebGLTileLayer;
 >>>>>>> 7349011 (added GeoTIFFTileLayer)
+=======
+  tileLayer: WebGLTileLayer;
+>>>>>>> 06de91b (ApiKey)
 }
