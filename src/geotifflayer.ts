073a0f6e1d2ea62ba37a8476cf9eedff864c249a
Nour-Cheour10 <nour.cheour@insat.ucar.tn>
2024-07-24 14:12:28 +0200

    fixed GeoTiff file

--- src/geotifflayer.ts
+++ src/geotifflayer.ts
@@ -6,8 +6,6 @@ import GeoTIFF from 'ol/source/GeoTIFF.js';
 import { MODULE_NAME, MODULE_VERSION } from './version';
 import { LayerModel, LayerView } from './layer';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 export class GeoTIFFTileLayerModel extends LayerModel {
   defaults() {
     return {
@@ -65,89 +63,4 @@ export class GeoTIFFTileLayerView extends LayerView {
   }
 
   obj: WebGLTileLayer;
-=======
-
-=======
->>>>>>> 06de91b (ApiKey)
-export class GeoTIFFTileLayerModel extends LayerModel {
-  defaults() {
-    return {
-      ...super.defaults(),
-      _model_name: GeoTIFFTileLayerModel.model_name,
-      _model_module: GeoTIFFTileLayerModel.model_module,
-      _model_module_version: GeoTIFFTileLayerModel.model_module_version,
-      _view_name: GeoTIFFTileLayerModel.view_name,
-      _view_module: GeoTIFFTileLayerModel.view_module,
-      _view_module_version: GeoTIFFTileLayerModel.view_module_version,
-      source: [],
-    };
-  }
-
-  static serializers: ISerializers = {
-    ...DOMWidgetModel.serializers,
-    // Add any extra serializers here
-  };
-
-  static model_name = 'GeoTIFFTileLayerModel';
-  static model_module = MODULE_NAME;
-  static model_module_version = MODULE_VERSION;
-  static view_name = 'GeoTIFFTileLayerView';
-  static view_module = MODULE_NAME;
-  static view_module_version = MODULE_VERSION;
-}
-
-export class GeoTIFFTileLayerView extends LayerView {
-  render() {
-    super.render();
-    this.sourcesChanged();
-    this.model.on('change:sources', this.sourcesChanged, this);
-  }
-
-  create_obj() {
-    const sources = this.model.get('sources').map((source: any) => ({
-      url: source.url,
-    }));
-
-    this.obj = this.tileLayer = new WebGLTileLayer({
-      source: new GeoTIFF({
-        sources: sources,
-      }),
-    });
-  }
-
-  sourcesChanged() {
-    const newSources = this.model.get('sources').map((source: any) => ({
-      url: source.url,
-    }));
-
-    if (newSources) {
-      const newSource = new GeoTIFF({
-        sources: newSources,
-      });
-      this.tileLayer.setSource(newSource);
-    }
-  }
-
-<<<<<<< HEAD
-
- sourcesChanged() {
-   const newSources = this.model.get('sources').map((source: any) => ({
-     url: source.url,
-   }));
-
-
-   if (newSources) {
-     const newSource = new GeoTIFF({
-       sources: newSources,
-     });
-     this.tileLayer.setSource(newSource);
-   }
- }
-
-
- tileLayer: WebGLTileLayer;
->>>>>>> 7349011 (added GeoTIFFTileLayer)
-=======
-  tileLayer: WebGLTileLayer;
->>>>>>> 06de91b (ApiKey)
 }
